c8aec01ec408ea3a2d18c9c5150fd00724dcf89a
XavierChanth <xchanthavong@gmail.com>
2021-06-08 23:59:04 -0400

    add config base-file

---
+++ config.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+packages="git curl xclip xdg-utils zip unzip" # ONLY PACMAN PACKAGES ALLOWED
+git_user_name=""
+git_user_email=""
+git_credential_helper=""
+ssh_keygen_type="ed25519"
+arch_fonts="ttf-fira-code noto-fonts noto-fonts-emoji"
+aur_fonts="ttf-ms-fonts"
+arch_apps="wireshark-qt vivaldi vivaldi-ffmpeg-codecs firefox"
+aur_apps="visual-studio-code-bin"
+android_image="system-images;android-30;google_apis;x86"
